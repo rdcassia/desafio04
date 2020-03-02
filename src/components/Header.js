a5aa8618a41c1e4c87be5d80eb772f6592747e14
Cassia <cassia@ssinformatica.net>
2020-03-02 09:42:19 -0300

    Start

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+
+  render() {
+    return (
+      <div class="header">
+        <label>Facebook</label>
+        <label>Meu perfil</label>
+      </div>
+    )
+  }
+}
+
+export default Header;
